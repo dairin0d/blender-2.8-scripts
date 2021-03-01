5190ea747c83bf03ecaf1f5b273006f2301efd25
dairin0d <dima.glib@gmail.com>
2021-03-01 14:48:45 +0200

    Mouselook Navigation: update the addon version

--- scripts/addons/mouselook_navigation/__init__.py
+++ scripts/addons/mouselook_navigation/__init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "Mouselook Navigation",
     "author": "dairin0d, moth3r",
-    "version": (1, 6, 0),
+    "version": (1, 6, 1),
     "blender": (2, 80, 0),
     "location": "View3D > orbit/pan/dolly/zoom/fly/walk",
     "description": "Provides extra 3D view navigation options (ZBrush mode) and customizability",
